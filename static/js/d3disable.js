429ea927abdd771634bacb63491d7e0a762d41f8
basilgeorge <basilgeorgetechy@gmail.com>
2022-03-19 16:57:21 +0530

    'day02

---
+++ static/js/d3disable.js
@@ -0,0 +1,249 @@
+$(document).ready(function() {
+
+    $("#frame00").click(function(){
+      $("#d2Modalframe00").modal({
+          backdrop: 'static',
+      });
+     });
+  
+    $("#frame01").click(function(){
+      $("#d2Modalframe01").modal({
+          backdrop: 'static',
+      });
+     });
+  
+    $("#frame02").click(function(){
+      $("#d2Modalframe02").modal({
+          backdrop: 'static',
+      });
+     });
+  
+     $("#frame03").click(function(){
+      $("#d2Modalframe03").modal({
+          backdrop: 'static',
+      });
+     });
+  
+     $("#frame04").click(function(){
+      $("#d2Modalframe04").modal({
+          backdrop: 'static',
+      });
+     });
+  
+     $("#frame05").click(function(){
+      $("#d2Modalframe05").modal({
+          backdrop: 'static',
+      });
+     });
+  
+     $("#frame06").click(function(){
+      $("#d2Modalframe06").modal({
+          backdrop: 'static',
+      });
+     });
+  
+     $("#frame07").click(function(){
+      $("#d2Modalframe07").modal({
+          backdrop: 'static',
+      });
+     });
+  
+     $("#frame08").click(function(){
+      $("#d2Modalframe08").modal({
+          backdrop: 'static',
+      });
+     });
+  
+     $("#frame09").click(function(){
+      $("#d2Modalframe09").modal({
+          backdrop: 'static',
+      });
+     });
+  
+     $("#frame10").click(function(){
+      $("#d2Modalframe10").modal({
+          backdrop: 'static',
+      });
+     });
+  
+     $("#frame11").click(function(){
+      $("#d2Modalframe11").modal({
+          backdrop: 'static',
+      });
+     });
+  
+     $("#frame12").click(function(){
+      $("#d2Modalframe12").modal({
+          backdrop: 'static',
+      });
+     });
+  
+     $("#frame13").click(function(){
+      $("#d2Modalframe14").modal({
+          backdrop: 'static',
+      });
+     });
+  
+     $("#frame15").click(function(){
+      $("#d2Modalframe15").modal({
+          backdrop: 'static',
+      });
+     });
+  
+     $("#frame16").click(function(){
+      $("#d2endframe").modal({
+          backdrop: 'static',
+      });
+     });
+  
+      $('#d2next').click(function() {
+        $(this).prop('disabled',true);
+        $("#d2content").show();
+  
+        setTimeout(function(){
+          $('#d2next').prop('disabled', false);
+          $("#d2content").hide();
+        },8000);
+     });
+  
+     $('#d2next2').click(function() {
+      $(this).prop('disabled',true);
+      $("#d2content2").show();
+  
+      setTimeout(function(){
+        $('#d2next2').prop('disabled', false);
+        $("#d2content2").hide();
+      },13000);
+   });
+  
+   $('#d2next3').click(function() {
+      $(this).prop('disabled',true);
+      $("#d2content3").show();
+  
+      setTimeout(function(){
+        $('#d2next3').prop('disabled', false);
+        $("#d2content3").hide();
+      },10000);
+   });
+  
+   $('#d2next4').click(function() {
+      $(this).prop('disabled',true);
+      $("#d2content4").show();
+  
+      setTimeout(function(){
+        $('#d2next4').prop('disabled', false);
+        $("#d2content4").hide();
+      },6000);
+   });
+  
+   $('#d2next5').click(function() {
+      $(this).prop('disabled',true);
+      $("#d2content5").show();
+  
+      setTimeout(function(){
+        $('#d2next5').prop('disabled', false);
+        $("#d2content5").hide();
+      },8000);
+   });
+  
+   $('#d2next6').click(function() {
+      $(this).prop('disabled',true);
+      $("#d2content6").show();
+  
+      setTimeout(function(){
+        $('#d2next6').prop('disabled', false);
+        $("#d2content6").hide();
+      },8000);
+   });
+  
+   $('#d2next7').click(function() {
+      $(this).prop('disabled',true);
+      $("#d2content7").show();
+  
+      setTimeout(function(){
+        $('#d2next7').prop('disabled', false);
+        $("#d2content7").hide();
+      },9000);
+   });
+  
+   $('#d2next8').click(function() {
+      $(this).prop('disabled',true);
+      $("#d2content8").show();
+  
+      setTimeout(function(){
+        $('#d2next8').prop('disabled', false);
+        $("#d2content8").hide();
+      },8000);
+   });
+  
+   $('#d2next9').click(function() {
+      $(this).prop('disabled',true);
+      $("#content9").show();
+  
+      setTimeout(function(){
+        $('#d2next9').prop('disabled', false);
+        $("#d2content9").hide();
+      },7000);
+   });
+  
+   $('#d2next10').click(function() {
+      $(this).prop('disabled',true);
+      $("#d2content10").show();
+  
+      setTimeout(function(){
+        $('#d2next10').prop('disabled', false);
+        $("#d2content10").hide();
+      },9000);
+   });
+  
+   $('#d2next11').click(function() {
+      $(this).prop('disabled',true);
+      $("#d2content11").show();
+  
+      setTimeout(function(){
+        $('#d2next11').prop('disabled', false);
+        $("#d2content11").hide();
+      },9000);
+   });
+  
+   $('#d2next12').click(function() {
+      $(this).prop('disabled',true);
+      $("#d2content12").show();
+  
+      setTimeout(function(){
+        $('#d2next12').prop('disabled', false);
+        $("#d2content12").hide();
+      },14000);
+   });
+  
+   $('#d2next13').click(function() {
+      $(this).prop('disabled',true);
+      $("#d2content13").show();
+  
+      setTimeout(function(){
+        $('#d2next13').prop('disabled', false);
+        $("#d2content13").hide();
+      },10000);
+   });
+  
+   $('#d2next14').click(function() {
+      $(this).prop('disabled',true);
+      $("#d2content14").show();
+  
+      setTimeout(function(){
+        $('#d2next14').prop('disabled', false);
+        $("#d2content14").hide();
+      },10000);
+   });
+  
+   $('#d2next15').click(function() {
+      $(this).prop('disabled',true);
+      $("#d2content15").show();
+  
+      setTimeout(function(){
+        $('#d2next15').prop('disabled', false);
+        $("#d2content15").hide();
+      },8000);
+   });
+  
+   });
---
+++ static/js/d3framecont.js
@@ -0,0 +1,177 @@
+{
+    function d2playpau00(){
+       document.getElementById("d2playpause00").hidden=true;
+       document.getElementById('d2myVideo00').play();
+    }
+ }
+ {
+    document.getElementById("d2task11").hidden=true;
+    document.getElementById("d2next11").disabled=true;
+    document.getElementById("d2playpause11").hidden=false;
+    document.getElementById('d2complete11').hidden=true;
+    var inc=1;
+    document.getElementById("d2count11").innerHTML ="         " + " Viewed :" + (inc) +" times";
+    function d2playpau11(){
+       document.getElementById("d2playpause11").hidden=true;
+       document.getElementById('d2myVideo11').play();
+       document.getElementById("d2next11").disabled=false;
+      }
+    function d2myFunction11() {
+       document.getElementById('d2myVideo11').play();
+       inc = inc+1;
+       document.getElementById("d2count11").innerHTML ="         " + " Viewed : " + (inc) + " times";
+       if(inc == 6 ) {
+        document.getElementById("d2task11").hidden=false;
+        document.getElementById("d2next11").hidden=true;
+        document.getElementById("d2hint11").hidden=true;
+        document.getElementById('d2myVideo11').hidden=true;
+        document.getElementById('d2myVideo11').pause();
+        document.getElementById("d2count11").hidden=true;
+        document.getElementById("d2content11").hidden=true;
+        document.getElementById("d2doubtimg11").hidden=true;
+        document.getElementById('d2complete11').hidden=false;
+        document.getElementById('d2complete11').play();
+       }      
+    }
+    $("#d2task11").click(function () {
+    document.location.reload();
+    }) 
+ }
+  {
+     document.getElementById("d2task12").hidden=true;
+     document.getElementById("d2next12").disabled=true;
+     document.getElementById("d2playpause12").hidden=false;
+     document.getElementById('d2complete12').hidden=true;
+     var inc=1;
+     document.getElementById("d2count12").innerHTML ="         " + " Viewed :" + (inc) +" times";
+     function d2playpau12(){
+        document.getElementById("d2playpause12").hidden=true;
+        document.getElementById('d2myVideo12').play();
+        document.getElementById("d2next12").disabled=false;
+       }
+     function d2myFunction12() {
+        document.getElementById('d2myVideo12').play();
+        inc = inc+1;
+        document.getElementById("d2count12").innerHTML ="         " + " Viewed : " + (inc) + " times";
+        if(inc == 6 ) {
+         document.getElementById("d2task12").hidden=false;
+         document.getElementById("d2next12").hidden=true;
+         document.getElementById("d2hint12").hidden=true;
+         document.getElementById('d2myVideo12').hidden=true;
+         document.getElementById('d2myVideo12').pause();
+         document.getElementById("d2count12").hidden=true;
+         document.getElementById("d2content12").hidden=true;
+         document.getElementById("d2doubtimg12").hidden=true;
+         document.getElementById('d2complete12').hidden=false;
+         document.getElementById('d2complete12').play();
+        }      
+     }
+     $("#d2task12").click(function () {
+     document.location.reload();
+     }) 
+  }
+  {
+     document.getElementById("d2task13").hidden=true;
+     document.getElementById("d2next13").disabled=true;
+     document.getElementById("d2playpause13").hidden=false;
+     document.getElementById('d2complete13').hidden=true;
+     var inc=1;
+     document.getElementById("d2count13").innerHTML ="         " + " Viewed :" + (inc) +" times";
+     function d2playpau13(){
+        document.getElementById("d2playpause13").hidden=true;
+        document.getElementById('d2myVideo13').play();
+        document.getElementById("d2next13").disabled=false;
+       }
+     function d2myFunction13() {
+        document.getElementById('d2myVideo13').play();
+        inc = inc+1;
+        document.getElementById("d2count13").innerHTML ="         " + " Viewed : " + (inc) + " times";
+        if(inc == 6 ) {
+         document.getElementById("d2task13").hidden=false;
+         document.getElementById("d2next13").hidden=true;
+         document.getElementById("d2hint13").hidden=true;
+         document.getElementById('d2myVideo13').hidden=true;
+         document.getElementById('d2myVideo13').pause();
+         document.getElementById("d2count13").hidden=true;
+         document.getElementById("d2content13").hidden=true;
+         document.getElementById("d2doubtimg13").hidden=true;
+         document.getElementById('d2complete13').hidden=false;
+         document.getElementById('d2complete13').play();
+        }      
+     }
+     $("#d2task13").click(function () {
+     document.location.reload();
+     }) 
+  }
+  {
+     document.getElementById("d2task14").hidden=true;
+     document.getElementById("d2next14").disabled=true;
+     document.getElementById("d2playpause14").hidden=false;
+     document.getElementById('d2complete14').hidden=true;
+     var inc=1;
+     document.getElementById("d2count14").innerHTML ="         " + " Viewed :" + (inc) +" times";
+     function d2playpau14(){
+        document.getElementById("d2playpause14").hidden=true;
+        document.getElementById('d2myVideo14').play();
+        document.getElementById("d2next14").disabled=false;
+       }
+     function d2myFunction14() {
+        document.getElementById('d2myVideo14').play();
+        inc = inc+1;
+        document.getElementById("d2count14").innerHTML ="         " + " Viewed : " + (inc) + " times";
+        if(inc == 6 ) {
+         document.getElementById("d2task14").hidden=false;
+         document.getElementById("d2next14").hidden=true;
+         document.getElementById("d2hint14").hidden=true;
+         document.getElementById('d2myVideo14').hidden=true;
+         document.getElementById('d2myVideo14').pause();
+         document.getElementById("d2count14").hidden=true;
+         document.getElementById("d2content14").hidden=true;
+         document.getElementById("d2doubtimg14").hidden=true;
+         document.getElementById('d2complete14').hidden=false;
+         document.getElementById('d2complete14').play();
+        }      
+     }
+     $("#d2task14").click(function () {
+     document.location.reload();
+     }) 
+  }
+  {
+     document.getElementById("d2task15").hidden=true;
+     document.getElementById("d2next15").disabled=true;
+     document.getElementById("d2playpause15").hidden=false;
+     document.getElementById('d2complete15').hidden=true;
+     var inc=1;
+     document.getElementById("d2count15").innerHTML ="         " + " Viewed :" + (inc) +" times";
+     function d2playpau15(){
+        document.getElementById("d2playpause15").hidden=true;
+        document.getElementById('d2myVideo15').play();
+        document.getElementById("d2next15").disabled=false;
+       }
+     function d2myFunction15() {
+        document.getElementById('d2myVideo15').play();
+        inc = inc+1;
+        document.getElementById("d2count15").innerHTML ="         " + " Viewed : " + (inc) + " times";
+        if(inc == 6 ) {
+         document.getElementById("d2task15").hidden=false;
+         document.getElementById("d2next15").hidden=true;
+         document.getElementById("d2hint15").hidden=true;
+         document.getElementById('d2myVideo15').hidden=true;
+         document.getElementById('d2myVideo15').pause();
+         document.getElementById("d2count15").hidden=true;
+         document.getElementById("d2content15").hidden=true;
+         document.getElementById("d2doubtimg15").hidden=true;
+         document.getElementById('d2complete15').hidden=false;
+         document.getElementById('d2complete15').play();
+        }      
+     }
+     $("#d2task15").click(function () {
+     document.location.reload();
+     }) 
+  }
+  {
+    function d2playpau16(){
+       document.getElementById("d2playpause16").hidden=true;
+       document.getElementById('d2myVideo16').play();
+    }
+ }
